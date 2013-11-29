e4d6c34c00a37cd576466bfebf1e8088008a73cd
mb <homer@awesom.eu>
2013-11-29 17:25:12 +0100

    adding log management similar to https://github.com/acieroid/goeland; thx acieroid

--- fortune.go
+++ fortune.go
@@ -68,21 +68,30 @@ func init() {
 }
 
 func main() {
+	output, err := os.OpenFile("fortune.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		log.Fatal("Cannot open fortune.log: ", err)
+	}
+	log.setOutput(output)
+
 	if len(os.Args) <= 1 {
 		err := loadfortunes("fortunes/9fortunes")
 		if err != nil {
-			fmt.Printf("%s\n", err)
+			log.Println("Error while parsing 9fortunes: %s\n", err)
 		}
 	}
 	for i := 1; i < len(os.Args); i++ {
 		err := loadfortunes(os.Args[i])
 		if err != nil {
-			fmt.Printf("%s\n", err)
+			log.Println("Error while parsing %s: %s\n", os.Args[i], err)
 		}
 	}
 
 	fmt.Printf("%d fortunes loaded\n", len(fortunes))
 
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8081", nil)
+	err = http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatal("Cannot start HTTP server: ", err)
+	}
 }
